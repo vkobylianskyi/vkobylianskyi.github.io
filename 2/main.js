e4058ecf16d448f026bb303fbcf7ca72a6609860
admin <volodymyr_kobylianskiy@ukr.net>
2018-07-17 19:46:13 +0300

    16.07.18

---
+++ 2/main.js
@@ -0,0 +1,26 @@
+var
+    myNumber = 20,
+    myString = "Some string",
+    myBool = true,
+    myNull = null,
+    myUndef = undefined,
+    anotherVar = 345;
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBool);
+console.log(typeof myUndef);
+
+console.log("Sorax".toUpperCase());
+
+var obj = {name: "Sorax"},
+    array = [1, 2, 3],
+    regexp = /w+/g,
+    func = function () {
+    };
+
+console.log("");
+console.log(typeof obj);
+console.log(typeof array);
+console.log(typeof regexp);
+console.log(typeof func);
