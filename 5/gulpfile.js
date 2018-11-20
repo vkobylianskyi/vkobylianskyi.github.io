af0e28a43014b97c8f34ff6473909b502ff92e14
admin <volodymyr_kobylianskiy@ukr.net>
2018-11-20 21:17:45 +0200

    20.11

--- 5/gulpfile.js
+++ 5/gulpfile.js
@@ -14,18 +14,21 @@ gulp.task('build-styles', () => {
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe(cssnano())
+        .pipe(browserSync.stream({match: '**/*.css'}))
         .pipe(gulp.dest('dist/'))
-        .pipe(livereload());
+        .pipe(browserSync.stream());
 });
 
 gulp.task('copy-assets', () => {
     return gulp.src('src/assets/**/*')
         .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('copy:libs', function() {
     gulp.src(npmDist(), {base: './node_modules'})
-        .pipe(gulp.dest('./dist/node_modules'));
+        .pipe(gulp.dest('./dist/node_modules'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('serve', () => {
@@ -40,12 +43,18 @@ gulp.task('serve', () => {
     gulp.watch('src/styles/*.scss', ['build-styles']);
     gulp.watch('src/index.html', ['copy-html']);
     gulp.watch('src/assets/**/*', ['copy-assets']);
-    gulp.watch('./node_modules', ['copy:libs'])
+    gulp.watch('./node_modules', ['copy:libs']);
+
 });
 
 gulp.task('copy-html', () => {
     return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('clear:dist', ()=> {
+
 });
 
 gulp.task('watch', ['build-styles', 'copy-html', 'copy-assets', 'copy:libs', 'serve']);
