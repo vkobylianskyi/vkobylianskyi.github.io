9cc5e190d264c6c551524d5f68c73090a30d8811
admin <volodymyr_kobylianskiy@ukr.net>
2019-02-15 18:48:55 +0200

    swiper bug

---
+++ projects/8/dist/main.js
@@ -0,0 +1,25 @@
+
+const currentWight = window.innerWidth;
+let slidesPerView;
+
+if (currentWight < 576) {
+    slidesPerView = 1;
+}
+
+if (currentWight < 768 && currentWight > 576)  {
+    slidesPerView = 2;
+}
+
+if (currentWight > 992)  {
+    slidesPerView = 4;
+}
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView,
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.swiper-angle-right',
+        prevEl: '.swiper-angle-left',
+      },
+  });
