d17ce3dfc769801f040ac7e40661650454899bba
Mike Rudenko <mihailo.rudenko.main@gmail.com>
2019-02-22 00:33:38 +0200

    fixed swiper

--- projects/8/src/main.js
+++ projects/8/src/main.js
@@ -1,26 +1,29 @@
+const init = () => {
+    const currentWight = window.innerWidth;
+    let slidesPerView;
 
-const currentWight = window.innerWidth;
-let slidesPerView;
+    if (currentWight < 576) {
+        slidesPerView = 1;
+    }
 
-if (currentWight < 576) {
-    slidesPerView = 1;
-}
+    if (currentWight < 768 && currentWight > 576) {
+        slidesPerView = 2;
+    }
 
-if (currentWight < 768 && currentWight > 576)  {
-    slidesPerView = 2;
-}
+    if (currentWight > 992) {
+        slidesPerView = 5;
+    }
 
-if (currentWight > 992)  {
-    slidesPerView = 4;
-}
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton:'.swiper-button-prev',
+        paginationType: 'bullets',
+        paginationClickable: true,
+    });
+};
 
-
-var swiper = new Swiper('.swiper', {
-    slidesPerView,
-    spaceBetween: 30,
-    centeredSlides: true,
-    navigation: {
-        nextEl: '.swiper-angle-right',
-        prevEl: '.swiper-angle-left',
-      },
-  });
+document.addEventListener('DOMContentLoaded', init);
