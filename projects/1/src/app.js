e0d3aa6fc544caf736ccb8738209b309e978c8e5
Mike Rudenko <mihailo.rudenko.main@gmail.com>
2019-03-03 13:07:53 +0200

    fixed proj 1

--- projects/1/src/app.js
+++ projects/1/src/app.js
@@ -1,3 +1,5 @@
-// import $ from "jquery";
+import $ from "jquery";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PopperJs from 'popper.js';
+const b = require('bootstrap');
 import './styles/style.scss';
