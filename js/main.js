e0508a249e331aaf4243d8468f0d8003645a039e
admin <volodymyr_kobylianskiy@ukr.net>
2019-05-08 14:38:30 +0300

    add favicon

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+
+	"use strict";	
+
+  
+    $('.navigation').singlePageNav({
+        currentClass : 'active'
+    });
+
+
+    $('.toggle-menu').click(function(){
+        $('.responsive-menu').stop(true,true).slideToggle();
+        return false;
+    });
+
+})(jQuery);
