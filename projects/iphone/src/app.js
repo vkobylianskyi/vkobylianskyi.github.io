6da4df156a419cb51972fb768fc2bdf2f6221acc
admin <volodymyr_kobylianskiy@ukr.net>
2019-05-22 19:29:45 +0300

    tesk task

--- projects/iphone/src/app.js
+++ projects/iphone/src/app.js
@@ -1,9 +1,24 @@
 import $ from 'jquery';
-import b from  'bootstrap';
+import Swiper from 'swiper';
+import b from 'bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'swiper/dist/css/swiper.min.css';
 import './styles/style.scss';
+
+
+
+(function () {
+    const swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
+        },
+    });
+})();
+
 
 // (function(){ß
 //     $(document).ready(startApp);
@@ -12,17 +27,17 @@ import './styles/style.scss';
 //         $(window).scroll(onScrollWindow);
 //         $('.top-button, .fa-angle-up').click(onTopBtnClick);
 //     }
-    
+
 //     function onLinkClick() {
 //         $('html, body').animate({
 //             scrollTop: $(this.hash).offset().top
 //         }, DELAY);
 //     }
-    
+
 //     function onTopBtnClick() {
 //         $('html,body').animate({ scrollTop: 0 }, DELAY)
 //     }
-    
+
 //     function onScrollWindow() {
 //         $(this).scrollTop() > 1000 
 //             ?  $('.top-button').fadeIn()
